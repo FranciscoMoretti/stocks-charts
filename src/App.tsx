a93332ebc1cbb8b8f4e4342b7e95ed193ed92cfd
Francisco Moretti <franciscoemoretti@gmail.com>
2024-04-15 12:38:12 +0100

    basic fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [stockData, setStockData] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `https://api.iex.cloud/v1/data/CORE/HISTORICAL_PRICES/SPY?token=${
+        import.meta.env.VITE_IEXCLOUD_SECRET_KEY
+      }`
+    )
+      .then((res) => {
+        return res.json().then((data) => {
+          setStockData(data);
+          console.log(data);
+        });
+      })
+
+      .catch((err) => console.error(err));
+  }, []);
 
   return (
     <>
@@ -29,7 +46,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

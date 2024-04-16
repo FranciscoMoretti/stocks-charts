eeab9eb7c35be73fe0ee94b3a18a12fbf58b65bd
Francisco Moretti <franciscoemoretti@gmail.com>
2024-04-16 11:54:51 +0100

    simple app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import { InputGroup } from "@blueprintjs/core";
 import "./App.css";
 import { Chart } from "./Chart";
 import { StockData } from "./api-types";
 
-function useStockPrice(tickerName: string) {
+function useStockPrice(tickerName: string, initialInterval = "1m") {
+  const [interval, setInterval] = useState(initialInterval);
   const [stockData, setStockData] = useState<StockData>([]);
   const [ticker, setTicker] = useState(tickerName);
   const [isLoading, setIsLoading] = useState(true);
@@ -19,13 +18,12 @@ function useStockPrice(tickerName: string) {
     fetch(
       `https://api.iex.cloud/v1/data/CORE/HISTORICAL_PRICES/${ticker}?token=${
         import.meta.env.VITE_IEXCLOUD_SECRET_KEY
-      }&range=1m`,
+      }&range=${interval}`,
       { signal }
     )
       .then((res) => {
         return res.json().then((data) => {
           setStockData(data);
-          console.log(data);
           setIsLoading(false);
         });
       })
@@ -40,9 +38,66 @@ function useStockPrice(tickerName: string) {
     return () => {
       abortController.abort();
     };
-  }, [ticker]);
+  }, [ticker, interval]);
 
-  return { stockData, ticker, setTicker, isLoading };
+  return { stockData, ticker, setTicker, isLoading, interval, setInterval };
+}
+
+function TickerSearcher({
+  ticker,
+  selectTicker,
+}: {
+  ticker: string;
+  selectTicker: (ticker: string) => void;
+}) {
+  const [tickers, setTickers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState(ticker);
+
+  useEffect(() => {
+    fetch(
+      `https://api.iex.cloud/v1/search/${searchTerm}?token=${
+        import.meta.env.VITE_IEXCLOUD_SECRET_KEY
+      }`
+    )
+      .then((res) => {
+        return res.json().then((data) => {
+          console.log(data);
+          setTickers(data.map((ticker: any) => ticker.symbol));
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, [searchTerm]);
+
+  return (
+    <div style={{ width: "500px" }}>
+      <InputGroup
+        large
+        placeholder="Search..."
+        type="search"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <ul style={{ width: "500px" }}>
+        {tickers.map((ticker) => {
+          return (
+            <li style={{ listStyle: "none" }}>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                {ticker}
+                <button
+                  style={{ background: "white", color: "black" }}
+                  onClick={() => selectTicker(ticker)}
+                >
+                  +
+                </button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 function App() {
@@ -51,13 +106,16 @@ function App() {
     ticker: ticker1,
     setTicker: setTicker1,
     isLoading: isLoading1,
-  } = useStockPrice("SPY");
+    setInterval: setInterval1,
+    interval,
+  } = useStockPrice("SPY", "1m");
   const {
     stockData: stockData2,
     ticker: ticker2,
     setTicker: setTicker2,
     isLoading: isLoading2,
-  } = useStockPrice("IRBT");
+    setInterval: setInterval2,
+  } = useStockPrice("IRBT", "1m");
 
   if (isLoading1 || isLoading2) {
     return <div>Loading...</div>;
@@ -74,17 +132,31 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <InputGroup />
+      <h1>Stocks Chart</h1>
       {chartData && <Chart stocks={chartData} />}
+      <div
+        style={{
+          display: "flex",
+          flex: "row",
+          gap: "12px",
+          justifyContent: "space-between",
+          alignItems: "start",
+        }}
+      >
+        <select
+          onChange={(e) => {
+            setInterval1(e.target.value);
+            setInterval2(e.target.value);
+          }}
+          value={interval}
+        >
+          <option value="1m">1m</option>
+          <option value="1y">1y</option>
+          <option value="5y">5y</option>
+        </select>
+        <TickerSearcher selectTicker={setTicker1} ticker={ticker1} />
+        <TickerSearcher selectTicker={setTicker2} ticker={ticker2} />
+      </div>
     </>
   );
 }

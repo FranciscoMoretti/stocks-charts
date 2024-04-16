d963f755441a751d078b10029b4238cd734237bd
Francisco Moretti <franciscoemoretti@gmail.com>
2024-04-16 10:35:59 +0100

    multiple tickers

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import { InputGroup } from "@blueprintjs/core";
 import "./App.css";
 import { Chart } from "./Chart";
+import { StockData } from "./api-types";
 
-function App() {
-  const [stockData, setStockData] = useState([]);
+function useStockPrice(tickerName: string) {
+  const [stockData, setStockData] = useState<StockData>([]);
+  const [ticker, setTicker] = useState(tickerName);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const abortController = new AbortController();
     const signal = abortController.signal;
+    setIsLoading(true);
 
     fetch(
-      `https://api.iex.cloud/v1/data/CORE/HISTORICAL_PRICES/SPY?token=${
+      `https://api.iex.cloud/v1/data/CORE/HISTORICAL_PRICES/${ticker}?token=${
         import.meta.env.VITE_IEXCLOUD_SECRET_KEY
       }&range=1m`,
       { signal }
@@ -21,6 +26,7 @@ function App() {
         return res.json().then((data) => {
           setStockData(data);
           console.log(data);
+          setIsLoading(false);
         });
       })
       .catch((err) => {
@@ -34,7 +40,37 @@ function App() {
     return () => {
       abortController.abort();
     };
-  }, []);
+  }, [ticker]);
+
+  return { stockData, ticker, setTicker, isLoading };
+}
+
+function App() {
+  const {
+    stockData: stockData1,
+    ticker: ticker1,
+    setTicker: setTicker1,
+    isLoading: isLoading1,
+  } = useStockPrice("SPY");
+  const {
+    stockData: stockData2,
+    ticker: ticker2,
+    setTicker: setTicker2,
+    isLoading: isLoading2,
+  } = useStockPrice("IRBT");
+
+  if (isLoading1 || isLoading2) {
+    return <div>Loading...</div>;
+  }
+
+  // Convert to format: {date: string, ticker1: number, ticker2: number}[]
+  const chartData = stockData1.map((dataPoint: any, index: number) => {
+    return {
+      date: dataPoint.date,
+      [ticker1]: dataPoint.close,
+      [ticker2]: stockData2[index].close,
+    };
+  });
 
   return (
     <>
@@ -47,10 +83,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <Chart data={stockData} />
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <InputGroup />
+      {chartData && <Chart stocks={chartData} />}
     </>
   );
 }
--- src/Chart.tsx
+++ src/Chart.tsx
@@ -8,15 +8,34 @@ import {
   CartesianGrid,
 } from "recharts";
 
-export function Chart({ data }: { data: StockData }) {
-  const dataByDate = data.sort((a, b) => {
+interface ChartProps {
+  stocks: { date: string } & Record<string, number>[];
+}
+
+const DIFFERENT_LINE_COLORS = ["#8884d8", "#4d8888", "#aa4d88", "#88aa4d"];
+
+export function Chart({ stocks }: ChartProps) {
+  const dataByDate = stocks.sort((a, b) => {
     return a.date - b.date;
   });
 
   return (
     <div className="" style={{ width: "100%" }}>
       <LineChart width={1200} height={400} data={dataByDate}>
-        <Line type="monotone" dataKey="close" stroke="#8884d8" />
+        {
+          // Create a Line for each stock
+          Object.keys(stocks[0])
+            .filter((key) => key !== "date")
+            .map((stock, i) => {
+              return (
+                <Line
+                  type="monotone"
+                  dataKey={stock}
+                  stroke={DIFFERENT_LINE_COLORS[i]}
+                />
+              );
+            })
+        }
         <CartesianGrid stroke="#ccc" />
         <XAxis dataKey="priceDate" />
         <YAxis />
